31a68d134561db88e4628fc9cc8dc1e002812c73
Susisu <susisu2413@yahoo.co.jp>
2017-04-16 02:28:30 +0900

    Improve test for definitions to check range

--- src/defs.test.js
+++ src/defs.test.js
@@ -6,19 +6,24 @@ import Defs from "./defs.js";
 
 describe("defs", () => {
   it("should be ordered and complete", () => {
-    const propValues = ["A", "F", "H", "N", "Na", "W"];
+    const minCodePoint = 0x0000;
+    const maxCodePoint = 0x10FFFF;
+    const propValues   = ["A", "F", "H", "N", "Na", "W"];
     let prev = null;
     for (const def of Defs) {
-      expect(def).has.a.property("start").that.is.a("number");
-      expect(def).has.a.property("end").that.is.a("number");
-      expect(def).has.a.property("prop").that.is.oneOf(propValues);
+      expect(def).to.have.property("start")
+        .that.is.within(minCodePoint, maxCodePoint);
+      expect(def).to.have.property("end")
+        .that.is.within(minCodePoint, maxCodePoint);
+      expect(def).to.have.property("prop")
+        .that.is.oneOf(propValues);
       if (prev) {
         expect(def.start).to.equal(prev.end + 1);
         expect(def.prop).not.to.equal(prev.prop);
       }
       prev = def;
     }
-    expect(Defs[0].start).to.equal(0x0000);
-    expect(Defs[Defs.length - 1].end).to.equal(0x10FFFF);
+    expect(Defs[0].start).to.equal(minCodePoint);
+    expect(Defs[Defs.length - 1].end).to.equal(maxCodePoint);
   });
 });
