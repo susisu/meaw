44ad262d733b42d2c3eafc9b9ddd7207655f3990
Susisu <susisu2413@yahoo.co.jp>
2019-04-20 21:46:05 +0900

    use `declare module` syntax

--- meaw.d.ts
+++ meaw.d.ts
@@ -1,11 +1,9 @@
-declare namespace meaw {
-    export type EastAsianWidth = "N" | "Na" | "W" | "F" | "H" | "A";
+declare module "meaw" {
+  export type EastAsianWidth = "N" | "Na" | "W" | "F" | "H" | "A";
 
-    export function getEAW(str: string, at?: number): EastAsianWidth;
-    export function computeWidth(
-        str: string,
-        widthMap?: { [P in EastAsianWidth]?: number }
-    ): number;
+  export function getEAW(str: string, at?: number): EastAsianWidth;
+  export function computeWidth(
+    str: string,
+    widthMap?: { [P in EastAsianWidth]?: number }
+  ): number;
 }
-
-export = meaw;
