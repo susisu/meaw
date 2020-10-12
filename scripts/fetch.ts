a627f361714aefb0c54a6e4f389b1549a874852f
Susisu <susisu2413@yahoo.co.jp>
2020-10-12 20:17:27 +0900

    fix(scripts): fix types

--- scripts/fetch.ts
+++ scripts/fetch.ts
@@ -1,5 +1,5 @@
-import axios from "axios";
 import fs from "fs";
+import fetch from "node-fetch";
 import path from "path";
 
 const SOURCE_URL = "https://www.unicode.org/Public/UCD/latest/ucd/EastAsianWidth.txt";
@@ -7,12 +7,13 @@ const TARGET_PATH = path.resolve(__dirname, "../data/EastAsianWidth.txt");
 
 const ENCODING = "utf-8";
 
-async function fetch(): Promise<void> {
-  const res = await axios.get(SOURCE_URL);
-  await fs.promises.writeFile(TARGET_PATH, res.data, { encoding: ENCODING });
+async function main(): Promise<void> {
+  const res = await fetch(SOURCE_URL);
+  const text = await res.text();
+  await fs.promises.writeFile(TARGET_PATH, text, { encoding: ENCODING });
 }
 
-fetch().catch(err => {
+main().catch(err => {
   // eslint-disable-next-line no-console
   console.error(err);
 });
