679caab839a4bece66f90b701efeffc42b78a595
Susisu <susisu2413@yahoo.co.jp>
2021-09-18 21:08:08 +0900

    chore(scripts): use `node:` imports

--- scripts/dump-eaw-version.ts
+++ scripts/dump-eaw-version.ts
@@ -1,6 +1,6 @@
-import fs from "fs";
-import path from "path";
-import url from "url";
+import fs from "node:fs";
+import path from "node:path";
+import url from "node:url";
 import { readVersion } from "./lib/eaw.js";
 
 const DIRNAME = path.dirname(url.fileURLToPath(import.meta.url));
--- scripts/fetch.ts
+++ scripts/fetch.ts
@@ -1,6 +1,6 @@
-import fs from "fs";
-import path from "path";
-import url from "url";
+import fs from "node:fs";
+import path from "node:path";
+import url from "node:url";
 import fetch from "node-fetch";
 
 const DIRNAME = path.dirname(url.fileURLToPath(import.meta.url));
