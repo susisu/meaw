20cc4e6e28425c7eb955f5e33eafba579028b3f3
Susisu <susisu2413@yahoo.co.jp>
2023-09-23 01:38:33 +0900

    chore: update jest config to use esm

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,11 @@ export default {
   collectCoverage: true,
   collectCoverageFrom: ["./src/**/*.ts", "!./src/**/*.spec.ts"],
   coverageDirectory: "coverage",
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
   transform: {
-    "\\.ts$": ["ts-jest", { tsconfig: "./tsconfig.test.json" }],
+    "\\.ts$": ["ts-jest", { tsconfig: "./tsconfig.test.json", useESM: true }],
   },
 };
