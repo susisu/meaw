13e17c296723de54492ce2756d99e8e14754f334
Susisu <susisu2413@yahoo.co.jp>
2023-09-29 00:53:48 +0900

    build: use tsup

---
+++ tsup.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  tsconfig: "./tsconfig.build.json",
+  entry: ["src/index.ts"],
+  outDir: "./lib",
+  format: ["esm", "cjs"],
+  splitting: false,
+  sourcemap: true,
+  dts: true,
+  clean: true,
+});
