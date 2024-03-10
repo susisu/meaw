aab38c0b9c9209b6e719b034415fde8eb20c8c4f
Susisu <susisu2413@yahoo.co.jp>
2024-03-10 15:04:30 +0900

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,93 +1,56 @@
-import { config, map } from "@susisu/eslint-config";
-import prettierConfig from "eslint-config-prettier";
+import { config } from "@susisu/eslint-config";
 import jestPlugin from "eslint-plugin-jest";
 import jestFormattingPlugin from "eslint-plugin-jest-formatting";
 import globals from "globals";
 
-export default [
-  ...map(
-    {
-      files: ["src/**/*.ts"],
-      ignores: ["src/defs.ts"],
+export default config({}, [
+  {
+    ignores: ["src/defs.ts"],
+  },
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      globals: globals.es2021,
     },
-    [
-      config.tsTypeChecked,
-      prettierConfig,
-      {
-        languageOptions: {
-          sourceType: "module",
-          parserOptions: {
-            project: "./tsconfig.json",
-          },
-          globals: globals.es2021,
-        },
-      },
-    ]
-  ),
-  ...map(
-    {
-      files: ["src/**/*.spec.ts", "src/**/__tests__/**/*.ts"],
+  },
+  {
+    files: ["src/**/*.spec.ts", "src/**/__tests__/**/*.ts"],
+    plugins: {
+      "jest": jestPlugin,
+      "jest-formatting": jestFormattingPlugin,
     },
-    [
-      {
-        plugins: {
-          "jest": jestPlugin,
-          "jest-formatting": jestFormattingPlugin,
-        },
-      },
-      {
-        languageOptions: {
-          globals: jestPlugin.environments.globals.globals,
-        },
-        rules: {
-          ...jestPlugin.configs.recommended.rules,
-          "jest-formatting/padding-around-after-all-blocks": "error",
-          "jest-formatting/padding-around-after-each-blocks": "error",
-          "jest-formatting/padding-around-before-all-blocks": "error",
-          "jest-formatting/padding-around-before-each-blocks": "error",
-          "jest-formatting/padding-around-describe-blocks": "error",
-          "jest-formatting/padding-around-test-blocks": "error",
-        },
-      },
-    ]
-  ),
-  ...map(
-    {
-      files: ["scripts/**/*.ts"],
+    languageOptions: {
+      globals: jestPlugin.environments.globals.globals,
     },
-    [
-      config.tsTypeChecked,
-      prettierConfig,
-      {
-        languageOptions: {
-          sourceType: "module",
-          parserOptions: {
-            project: "./tsconfig.scripts.json",
-          },
-          globals: {
-            ...globals.es2021,
-            ...globals.node,
-          },
-        },
-      },
-    ]
-  ),
-  ...map(
-    {
-      files: ["*.js"],
+    rules: {
+      ...jestPlugin.configs.recommended.rules,
+      "jest-formatting/padding-around-after-all-blocks": "error",
+      "jest-formatting/padding-around-after-each-blocks": "error",
+      "jest-formatting/padding-around-before-all-blocks": "error",
+      "jest-formatting/padding-around-before-each-blocks": "error",
+      "jest-formatting/padding-around-describe-blocks": "error",
+      "jest-formatting/padding-around-test-blocks": "error",
     },
-    [
-      config.js,
-      prettierConfig,
-      {
-        languageOptions: {
-          sourceType: "module",
-          globals: {
-            ...globals.es2021,
-            ...globals.node,
-          },
-        },
+  },
+  {
+    files: ["scripts/**/*.ts"],
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.scripts.json",
       },
-    ]
-  ),
-];
+      globals: {
+        ...globals.es2021,
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ["*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.es2021,
+        ...globals.node,
+      },
+    },
+  },
+]);
