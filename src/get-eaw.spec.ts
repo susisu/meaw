91ce306146593852e76350ca27d7d1ff6d283e90
Susisu <susisu2413@yahoo.co.jp>
2024-03-10 15:16:01 +0900

    style: reformat

--- src/get-eaw.spec.ts
+++ src/get-eaw.spec.ts
@@ -53,7 +53,7 @@ describe("getEAW", () => {
       "should return the EAW property of the character at the specified position / %d",
       (pos, expected) => {
         expect(getEAW("ℵAあＡｱ∀", pos)).toBe(expected);
-      }
+      },
     );
 
     it.each([
