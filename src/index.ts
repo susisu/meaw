07c44c36b7c49b5384923a1ae0d2e07f2e7ba230
Susisu <susisu2413@yahoo.co.jp>
2025-03-23 15:46:12 +0900

    refactor: use export type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { EastAsianWidth } from "./types";
+export type { EastAsianWidth } from "./types";
 export { version as eawVersion } from "./defs";
 export { getEAW, getEAWOfCodePoint } from "./get-eaw";
 export { computeWidth } from "./compute-width";
