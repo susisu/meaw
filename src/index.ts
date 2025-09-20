4a85c1e7b1483fa2469d3c38198ccff364ea3b47
Susisu <susisu2413@yahoo.co.jp>
2025-09-20 18:52:01 +0900

    fix: ignore no-deprecated as it is expected

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export type { EastAsianWidth } from "./types";
 export { version as eawVersion } from "./defs";
 export { getEAW, getEAWOfCodePoint } from "./get-eaw";
+// eslint-disable-next-line @typescript-eslint/no-deprecated
 export { computeWidth } from "./compute-width";
