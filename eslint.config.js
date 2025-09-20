5b4748b053a79980315b1a897420ad99bc181a85
Susisu <susisu2413@yahoo.co.jp>
2025-09-20 19:08:12 +0900

    chore: set tsconfigRootDir

--- eslint.config.js
+++ eslint.config.js
@@ -2,43 +2,48 @@ import { config } from "@susisu/eslint-config";
 import vitestPlugin from "@vitest/eslint-plugin";
 import globals from "globals";
 
-export default config({}, [
+export default config(
   {
-    ignores: ["src/defs.ts"],
+    tsconfigRootDir: import.meta.dirname,
   },
-  {
-    files: ["src/**/*.ts"],
-    languageOptions: {
-      globals: {
-        ...globals.es2023,
+  [
+    {
+      ignores: ["src/defs.ts"],
+    },
+    {
+      files: ["src/**/*.ts"],
+      languageOptions: {
+        globals: {
+          ...globals.es2023,
+        },
       },
     },
-  },
-  {
-    files: ["src/**/*.spec.ts"],
-    plugins: {
-      vitest: vitestPlugin,
-    },
-    rules: {
-      ...vitestPlugin.configs.recommended.rules,
-    },
-  },
-  {
-    files: ["scripts/**/*.ts"],
-    languageOptions: {
-      globals: {
-        ...globals.es2023,
-        ...globals.node,
+    {
+      files: ["src/**/*.spec.ts"],
+      plugins: {
+        vitest: vitestPlugin,
+      },
+      rules: {
+        ...vitestPlugin.configs.recommended.rules,
       },
     },
-  },
-  {
-    files: ["*.js"],
-    languageOptions: {
-      globals: {
-        ...globals.es2023,
-        ...globals.node,
+    {
+      files: ["scripts/**/*.ts"],
+      languageOptions: {
+        globals: {
+          ...globals.es2023,
+          ...globals.node,
+        },
       },
     },
-  },
-]);
+    {
+      files: ["*.js"],
+      languageOptions: {
+        globals: {
+          ...globals.es2023,
+          ...globals.node,
+        },
+      },
+    },
+  ],
+);
