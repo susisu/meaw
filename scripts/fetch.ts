603d96be8239276686f49b7cfe3b23a0b762f80a
Susisu <susisu2413@yahoo.co.jp>
2025-11-01 18:22:58 +0900

    chore: add retry logic to fetch

--- scripts/fetch.ts
+++ scripts/fetch.ts
@@ -1,6 +1,7 @@
 import fs from "node:fs/promises";
 import path from "node:path";
 import url from "node:url";
+import pRetry from "p-retry";
 
 const DIRNAME = path.dirname(url.fileURLToPath(import.meta.url));
 const SOURCE_URL = "https://www.unicode.org/Public/UCD/latest/ucd/EastAsianWidth.txt";
@@ -9,7 +10,13 @@ const TARGET_PATH = path.resolve(DIRNAME, "../data/EastAsianWidth.txt");
 const ENCODING = "utf-8";
 
 async function main(): Promise<void> {
-  const res = await fetch(SOURCE_URL);
+  const res = await pRetry(async () => {
+    const res = await fetch(SOURCE_URL);
+    if (!res.ok) {
+      throw new Error(`Failed to fetch: ${res.status} ${res.statusText}`);
+    }
+    return res;
+  });
   const text = await res.text();
   await fs.writeFile(TARGET_PATH, text, { encoding: ENCODING });
 }
