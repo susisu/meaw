f373e497841d19b4c4bd12117d035e5613a9c410
Susisu <susisu2413@yahoo.co.jp>
2026-09-19 23:15:31 +0900

    refactor(test): simplify defs.spec.ts

--- src/defs.spec.ts
+++ src/defs.spec.ts
@@ -1,29 +1,15 @@
 import { describe, it, expect } from "vitest";
-import type { EAWDef } from "./types";
 import { defs } from "./defs";
 
 describe("defs", () => {
   it("should be ordered and complete", () => {
     const minCodePoint = 0x0000;
     const maxCodePoint = 0x10ffff;
-    const propValues = ["A", "F", "H", "N", "Na", "W"];
-    let prev: EAWDef | undefined = undefined;
-    for (const def of defs) {
-      const [start, end, prop] = def;
-      expect(start).toBeGreaterThanOrEqual(minCodePoint);
-      expect(start).toBeLessThanOrEqual(maxCodePoint);
-      expect(end).toBeGreaterThanOrEqual(minCodePoint);
-      expect(end).toBeLessThanOrEqual(maxCodePoint);
-      expect(propValues).toContain(prop);
-      if (prev) {
-        const [, prevEnd, prevProp] = prev;
 
-        expect(start).toBe(prevEnd + 1);
-        expect(prop).not.toBe(prevProp);
-      }
-      prev = def;
-    }
     expect(defs[0][0]).toBe(minCodePoint);
+    expect(defs.every((def, i) => i === 0 || defs[i - 1][1] + 1 === def[0])).toBe(true);
     expect(defs[defs.length - 1][1]).toBe(maxCodePoint);
+
+    expect(defs.every((def, i) => i === 0 || defs[i - 1][2] !== def[2])).toBe(true);
   });
 });
